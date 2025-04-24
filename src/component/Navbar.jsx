e00e10c1007c6e8be959d81ca68504a05e6e784d
shreyaNeupane <shreyaneupane49@gmail.com>
2025-04-24 14:42:05 +0545

    first commit

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="logo">
+            <img src="" alt="" srcset="" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
